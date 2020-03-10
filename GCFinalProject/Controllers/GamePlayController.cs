8b1383cd7a8632deadc05bd89c65867b95df1411
Jason <jasonrizzo02@aol.com>
2020-03-10 17:32:24 -0400

    adding logic to game controller

---
+++ GCFinalProject/Controllers/GamePlayController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GCFinalProject.Controllers
+{
+    public class GamePlayController : Controller
+    {
+        public IActionResult Index()
+        {
+            //call api
+            //keep track of correct answer IF ELSE
+            //if passing give them food/poison based on percentage
+            //auto feed after each quiz--based on diff mult total possible by percent right
+            //display avatar face on bottom-call database
+            //display quiz on top
+            //layer background
+            //ex give controls to mouse in jquery
+            //select difficult to image
+            //timer on quiz based on difficulty and questions
+            //extra music
+            //fancy highligt when at 100  
+
+            return View();
+        }
+    }
+}
